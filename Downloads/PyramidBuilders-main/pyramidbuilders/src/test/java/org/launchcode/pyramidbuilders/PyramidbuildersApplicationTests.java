1239600d64b407082e3269cc6ec20bb252cc0834
sjsbee <93555209+sjsbee@users.noreply.github.com>
2022-04-27 15:37:05 -0500

    added base sign-up screen

---
+++ Downloads/PyramidBuilders-main/pyramidbuilders/src/test/java/org/launchcode/pyramidbuilders/PyramidbuildersApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.pyramidbuilders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PyramidbuildersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
