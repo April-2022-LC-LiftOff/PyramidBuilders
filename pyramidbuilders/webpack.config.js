405e4a7574d2bfda993670ead6fdd501916d27f8
Rosalyn <rosalyn.stevenor@gmail.com>
2022-04-28 18:21:07 -0500

    incorporated CSS for easier style refactoring.

--- pyramidbuilders/webpack.config.js
+++ pyramidbuilders/webpack.config.js
@@ -4,14 +4,18 @@ module.exports = {
         filename: 'react-app.js'
     },
     module: {
-        rules: [{
-            test: /\.(js|jsx)$/,
-            exclude: /node_modules/,
-            loader: "babel-loader",
-            options: {
-                presets: ['@babel/preset-env', '@babel/preset-react']
-            }
-        }]
+         rules: [{
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: "babel-loader",
+                options: {
+                    presets: ['@babel/preset-env', '@babel/preset-react']
+                }
+            }, {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: ['style-loader', 'css-loader']
+            }]
     },
     resolve: {
         extensions: ['.js', '.jsx']
