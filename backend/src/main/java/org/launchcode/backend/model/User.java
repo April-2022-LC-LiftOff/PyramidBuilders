5120a64e2f5cffe0667e953ecb61f97ecfab0518
Roz S <93357876+rstevenor@users.noreply.github.com>
2022-05-01 12:07:35 -0500

    Able to add user to TemporaryDataBase repository through api post call.

---
+++ backend/src/main/java/org/launchcode/backend/model/User.java
@@ -0,0 +1,50 @@
+package org.launchcode.backend.model;
+
+public class User extends AbstractEntity {
+
+    private String username;
+    private String email;
+    private String password;
+
+    public User(){}
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    //for demonstration purposes
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
