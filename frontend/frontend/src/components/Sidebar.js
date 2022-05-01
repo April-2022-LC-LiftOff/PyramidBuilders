2ce4e2d47e74607524c5d40d99cb0f9a475037e2
Roz S <93357876+rstevenor@users.noreply.github.com>
2022-05-01 09:59:30 -0500

    finalized change to new project structure

---
+++ frontend/frontend/src/components/Sidebar.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import '../App.css';
+import {SidebarData} from './SidebarData'
+
+
+
+function Sidebar() {
+  return (
+    <div className="Sidebar">
+      <ul className="SidebarList">
+        {SidebarData.map((val, key) => {
+          return (
+            <li 
+              key={key}
+              className="row"
+              id={window.location.pathName == val.link ? "active" : ""}
+              onClick={()=> {window.location.pathname = val.link;
+              }}
+            >
+              <div id="icon">{val.icon}</div> <div id="title">{val.title}</div>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
+
+export default Sidebar
---
+++ frontend/frontend/src/components/TopbarData.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import * as FaIcons from 'react-icons/fa';
+
+export const TopbarData = [
+    {
+        title: 'Login',
+        // icon: <FaIcons.FaHome />,
+        link: '/login'
+    },
+    {
+        title: 'Register',
+        // icon: <FaIcons.FaUser />,
+        link: '/register'
+    }
+];
