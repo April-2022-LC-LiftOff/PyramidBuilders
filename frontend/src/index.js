cb9e6f00575a864fa214d49e1dcd9be86aefa1f8
KarakiBusiness <93557122+KarakiBusiness@users.noreply.github.com>
2022-05-03 21:26:17 -0400

    footer installed

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import "./components/Footer/index.css";
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
