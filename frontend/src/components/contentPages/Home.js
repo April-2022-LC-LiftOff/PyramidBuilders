d11449ad3ac737b9cf26b2334190b867b58cf939
Bunny McIntyre <bunnymcintyre@Bunnys-MacBook-Pro.local>
2022-05-07 13:06:16 -0500

    added basic cards to homepage

--- frontend/src/components/contentPages/Home.js
+++ frontend/src/components/contentPages/Home.js
@@ -1,13 +1,47 @@
 import React from "react";
-
-
+import Card from "react-bootstrap/Card";
+import Button from 'react-bootstrap/Button';
+import './pages.css';
 
 const Home = () => {
 	return (
+
+		
 		<div className="page">
-			<h1>Home Page</h1>
-			<p>This is the home page.</p>
+
+			<div>
+				<h1>Searchbar</h1>
+			</div>
+
+
+			<div className="container">
+		
+			<Card className="item" style={{ width: '18rem' }}>
+				<Card.Title id="title">Review of Movie</Card.Title>
+				<Card.Body id="body"> Some quick example text to build on the card title and make up the bulk of the card's content.</Card.Body>
+				<Button variant="primary">Go somewhere</Button>
+			</Card>
+	
+			<Card className="item" style={{ width: '18rem' }}>
+				<Card.Title id="title">Review of Movie</Card.Title>
+				<Card.Body id="body"> Some quick example text to build on the card title and make up the bulk of the card's content.</Card.Body>
+				<Button variant="primary">Go somewhere</Button>
+			</Card>
+			
+			<Card className="item" style={{ width: '18rem' }}>
+				<Card.Title id="title">Review of Movie</Card.Title>
+				<Card.Body id="body"> Some quick example text to build on the card title and make up the bulk of the card's content.</Card.Body>
+				<Button variant="primary">Go somewhere</Button>
+			</Card>
+			<Card className="item" style={{ width: '18rem' }}>
+				<Card.Title id="title">Review of Movie</Card.Title>
+				<Card.Body id="body"> Some quick example text to build on the card title and make up the bulk of the card's content.</Card.Body>
+				<Button variant="primary">Go somewhere</Button>
+			</Card>
+			
+			</div>
 		</div>
+
 	);
 };
 
