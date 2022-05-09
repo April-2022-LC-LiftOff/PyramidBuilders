80150169fdd97c8bf94489199c49d875b9cecd25
Bunny McIntyre <bunnymcintyre@Bunnys-MacBook-Pro.local>
2022-05-09 13:39:38 -0500

    added searchbar with submit icon; updated color cheme

--- frontend/src/components/contentPages/Home.js
+++ frontend/src/components/contentPages/Home.js
@@ -1,6 +1,9 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 import Button from 'react-bootstrap/Button';
+import * as FaIcons from 'react-icons/fa';
+import Form from 'react-bootstrap/Form'
+
 import './pages.css';
 
 const Home = () => {
@@ -9,8 +12,15 @@ const Home = () => {
 		
 		<div className="page">
 
-			<div>
-				<h1>Searchbar</h1>
+			<div>				
+			<Form className="searchContainer">
+				<Form.Group>
+					<Form.Control className="searchInput" type="text" placeholder="Search reviews" />
+				</Form.Group>
+				<Button id="searchIcon" variant="primary" type="submit">
+					<FaIcons.FaSearch />
+				</Button>
+			</Form>	 
 			</div>
 
 
@@ -19,24 +29,24 @@ const Home = () => {
 			<Card className="item" style={{ width: '18rem' }}>
 				<Card.Title id="title">Review of Movie</Card.Title>
 				<Card.Body id="body"> Some quick example text to build on the card title and make up the bulk of the card's content.</Card.Body>
-				<Button variant="primary">Go somewhere</Button>
+				<Button id="button" variant="primary">Go somewhere</Button>
 			</Card>
 	
 			<Card className="item" style={{ width: '18rem' }}>
 				<Card.Title id="title">Review of Movie</Card.Title>
 				<Card.Body id="body"> Some quick example text to build on the card title and make up the bulk of the card's content.</Card.Body>
-				<Button variant="primary">Go somewhere</Button>
+				<Button id="button" variant="primary">Go somewhere</Button>
 			</Card>
 			
 			<Card className="item" style={{ width: '18rem' }}>
 				<Card.Title id="title">Review of Movie</Card.Title>
 				<Card.Body id="body"> Some quick example text to build on the card title and make up the bulk of the card's content.</Card.Body>
-				<Button variant="primary">Go somewhere</Button>
+				<Button id="button" variant="primary">Go somewhere</Button>
 			</Card>
 			<Card className="item" style={{ width: '18rem' }}>
 				<Card.Title id="title">Review of Movie</Card.Title>
 				<Card.Body id="body"> Some quick example text to build on the card title and make up the bulk of the card's content.</Card.Body>
-				<Button variant="primary">Go somewhere</Button>
+				<Button id="button" variant="primary">Go somewhere</Button>
 			</Card>
 			
 			</div>
