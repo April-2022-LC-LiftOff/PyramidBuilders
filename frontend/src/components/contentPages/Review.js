bcb7c33ded987c20a7f85f98b2d93fc9f1c79519
Bunny McIntyre <bunnymcintyre@Bunnys-MacBook-Pro.local>
2022-05-09 15:25:01 -0500

    added rating to form; edited CSS styling

--- frontend/src/components/contentPages/Review.js
+++ frontend/src/components/contentPages/Review.js
@@ -19,6 +19,15 @@ const Review =() => {
                     </Form.Select>
                 </Form.Group>
 
+                <Form.Group className="reviewButtons" style={{ display: 'flex' }}>
+                    <Form.Label >Choose Your Rating</Form.Label>
+                    <Form.Check  label="1" type="radio"></Form.Check>
+                    <Form.Check  label="2" type="radio"></Form.Check>
+                    <Form.Check  label="3" type="radio"></Form.Check>
+                    <Form.Check  label="4" type="radio"></Form.Check>
+                    <Form.Check  label="5" type="radio"></Form.Check>
+                </Form.Group>
+
                 <Form.Group className="writeReview">
 					<Form.Control as="textarea" type="text" placeholder="Write your review here" />
 				</Form.Group>
