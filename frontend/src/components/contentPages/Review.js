34b6d71b4db822feb86fd84e1eaed2b583dd56d7
Bunny McIntyre <bunnymcintyre@Bunnys-MacBook-Pro.local>
2022-05-09 15:01:06 -0500

    updated CSS styling on write-review page

--- frontend/src/components/contentPages/Review.js
+++ frontend/src/components/contentPages/Review.js
@@ -8,10 +8,10 @@ const Review =() => {
     return (
         
         <div className="page">
-            <Form>
+            <Form className="reviewStyling">
 
-                <Form.Group>
-                    <Form.Label>Select movie to review</Form.Label>
+                <Form.Group className="selectMovie">
+                    <Form.Label className="selectLabel">Select a movie to review</Form.Label>
                     <Form.Select placeholder="Select a movie">
                        <option value="1">Movie 1 from API</option>
                        <option value="2">Movie 2 from API</option>
@@ -19,11 +19,11 @@ const Review =() => {
                     </Form.Select>
                 </Form.Group>
 
-                <Form.Group>
+                <Form.Group className="writeReview">
 					<Form.Control as="textarea" type="text" placeholder="Write your review here" />
 				</Form.Group>
                 
-                <Button type="submit"> Save Review </Button>
+                <Button className="submitReview" type="submit"> Save Review </Button>
             
             </Form>
       </div>
