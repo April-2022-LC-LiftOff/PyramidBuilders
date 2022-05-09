e43963710d8ba651611045d9a6c73d6d6f4e3aaa
Bunny McIntyre <bunnymcintyre@Bunnys-MacBook-Pro.local>
2022-05-09 14:51:39 -0500

    added basic review input form at /review

--- frontend/src/components/contentPages/Review.js
+++ frontend/src/components/contentPages/Review.js
@@ -9,24 +9,22 @@ const Review =() => {
         
         <div className="page">
             <Form>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
-                    <Form.Text className="text-muted">
-                    We'll never share your email with anyone else.
-                    </Form.Text>
+
+                <Form.Group>
+                    <Form.Label>Select movie to review</Form.Label>
+                    <Form.Select placeholder="Select a movie">
+                       <option value="1">Movie 1 from API</option>
+                       <option value="2">Movie 2 from API</option>
+                       <option value="3">Movie 3 from API</option>
+                    </Form.Select>
                 </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Check me out" />
-                </Form.Group>
-                <Button variant="primary" type="submit">
-                    Submit
-                </Button>
+                <Form.Group>
+					<Form.Control as="textarea" type="text" placeholder="Write your review here" />
+				</Form.Group>
+                
+                <Button type="submit"> Save Review </Button>
+            
             </Form>
       </div>
     )
