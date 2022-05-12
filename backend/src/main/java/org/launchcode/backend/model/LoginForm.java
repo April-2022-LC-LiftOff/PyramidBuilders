aaa9028adab7b486cbf5f1133b5f16633976710f
Roz S <93357876+rstevenor@users.noreply.github.com>
2022-05-12 17:21:54 -0500

    finished registration method that returns a HTTP response with Authorization header. Included an automatic sign in upon registration

---
+++ backend/src/main/java/org/launchcode/backend/model/LoginForm.java
@@ -0,0 +1,11 @@
+package org.launchcode.backend.model;
+
+public class LoginForm {
+    private String username;
+    private String password;
+
+    public LoginForm(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+}
