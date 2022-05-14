3bfdbf4b9a4ac184fd99e5fbcba497dfc2831830
Roz S <93357876+rstevenor@users.noreply.github.com>
2022-05-14 01:30:18 -0500

    backend authentication confirmation added to protect backend creation and retrival

--- backend/src/main/java/org/launchcode/backend/config/WebSecurityConfiguration.java
+++ backend/src/main/java/org/launchcode/backend/config/WebSecurityConfiguration.java
@@ -1,39 +1,39 @@
-package org.launchcode.backend.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
-
-import java.util.Collection;
-import java.util.Optional;
-import java.util.stream.Collectors;
-
-@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
-public class WebSecurityConfiguration  extends WebSecurityConfigurerAdapter {
-
-    //Decodes and verifies JWT tokens
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
-
-        http.oauth2ResourceServer()
-                .jwt()
-                .jwtAuthenticationConverter(jwtAuthenticationConverter());
-    }
-
-    public JwtAuthenticationConverter jwtAuthenticationConverter() {
-        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
-
-        converter.setJwtGrantedAuthoritiesConverter(jwt ->
-                Optional.ofNullable(jwt.getClaimAsStringList("custom_claims"))
-                        .stream()
-                        .flatMap(Collection::stream)
-                        .map(SimpleGrantedAuthority::new)
-                        .collect(Collectors.toList()));
-
-        return converter;
-    }
-}
+//package org.launchcode.backend.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.core.authority.SimpleGrantedAuthority;
+//import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+//
+//import java.util.Collection;
+//import java.util.Optional;
+//import java.util.stream.Collectors;
+//
+//@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
+//public class WebSecurityConfiguration  extends WebSecurityConfigurerAdapter {
+//
+//    //Decodes and verifies JWT tokens
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.csrf().disable();
+//
+//        http.oauth2ResourceServer()
+//                .jwt()
+//                .jwtAuthenticationConverter(jwtAuthenticationConverter());
+//    }
+//
+//    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+//        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+//
+//        converter.setJwtGrantedAuthoritiesConverter(jwt ->
+//                Optional.ofNullable(jwt.getClaimAsStringList("custom_claims"))
+//                        .stream()
+//                        .flatMap(Collection::stream)
+//                        .map(SimpleGrantedAuthority::new)
+//                        .collect(Collectors.toList()));
+//
+//        return converter;
+//    }
+//}
