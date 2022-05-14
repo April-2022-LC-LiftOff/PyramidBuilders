9c4ea7ca2dce2883f9a876607c78ad9625cefb5a
Roz S <93357876+rstevenor@users.noreply.github.com>
2022-05-14 11:50:35 -0500

    commiting to be able to checkout different branches

---
+++ pyramidbuilders/src/main/java/org/launchcode/pyramidbuilders/PyramidbuildersApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.pyramidbuilders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PyramidbuildersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PyramidbuildersApplication.class, args);
+	}
+
+}
