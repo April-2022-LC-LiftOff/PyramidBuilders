b1e608949f02c2c0981ff9eeebc5e51db6bcd1f5
sjsbee <93555209+sjsbee@users.noreply.github.com>
2022-05-20 14:55:42 -0500

    added login and registration pages

---
+++ Downloads/PyramidBuilders-main/frontend/src/components/SearchFunctions/CardList.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Card from "./Card";
+import "./SearchMovies.css";
+
+
+function CardList( {results}) {
+    let data = [];
+    if (results.data) {
+        data = results.data.Search || [];
+    }
+
+    return (
+        <div className="result" className="grid" >
+            {data.map((item) => (
+                <Card className="item" key={item.imdbID} movie={item} />
+            ))}
+        </div>
+    );
+}
+
+export default CardList;
