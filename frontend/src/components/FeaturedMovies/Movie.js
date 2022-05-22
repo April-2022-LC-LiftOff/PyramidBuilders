b3a18e9d8330a36f9aeb166efa553e04b8ddd6ce
Bunny McIntyre <bunnymcintyre@Bunnys-MacBook-Pro.local>
2022-05-22 16:28:32 -0500

    pulls static featured movie list on homepage

---
+++ frontend/src/components/FeaturedMovies/Movie.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import "../SearchFunctions/SearchMovies.css";
+
+const Movie = ({Title, Poster}) => (
+    
+    <div className="grid" className="cards" className="album">
+
+    <Card className="card" style={{ width: '18rem' }} className="box">
+        <Card.Img className="image" src={Poster}></Card.Img>
+        <Card.Title id="title">{Title}</Card.Title>
+    </Card>
+
+
+</div>
+);
+
+export default Movie;
