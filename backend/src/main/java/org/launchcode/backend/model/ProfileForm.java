f88d24873cea434de2d4e12475732b63438e5563
Roz S <93357876+rstevenor@users.noreply.github.com>
2022-06-01 21:07:42 -0500

    ran an npm prune on the node_module data still on main branch, added profile form for easier data retrieval.

---
+++ backend/src/main/java/org/launchcode/backend/model/ProfileForm.java
@@ -0,0 +1,18 @@
+package org.launchcode.backend.model;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.UserRecord;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashMap;
+
+@Getter
+@Setter
+public class ProfileForm extends Profile {
+
+    private String PhotoURL;
+    private String password;
+
+}
