47caaddd1ef5af32ac7e48576853f37f416f4da4
Roz S <93357876+rstevenor@users.noreply.github.com>
2022-06-08 22:11:08 -0500

    AverageRating api added

--- backend/src/main/java/org/launchcode/backend/model/Ratings.java
+++ backend/src/main/java/org/launchcode/backend/model/Ratings.java
@@ -1,9 +1,19 @@
 package org.launchcode.backend.model;
 
 public enum Ratings {
-    one,
-    two,
-    three,
-    four,
-    five
+    ONE(1),
+    TWO(2),
+    THREE(3),
+    FOUR(4),
+    FIVE(5);
+
+    private final int value;
+
+    private Ratings(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
 }
--- backend/src/main/java/org/launchcode/backend/services/ReviewService.java
+++ backend/src/main/java/org/launchcode/backend/services/ReviewService.java
@@ -3,6 +3,7 @@ package org.launchcode.backend.services;
 import com.google.api.core.ApiFuture;
 import com.google.cloud.firestore.*;
 import com.google.firebase.cloud.FirestoreClient;
+import org.launchcode.backend.model.Ratings;
 import org.launchcode.backend.model.Review;
 import org.launchcode.backend.model.ReviewForm;
 import org.springframework.stereotype.Service;
@@ -55,6 +56,28 @@ public class ReviewService {
         }
 
         return list;
+    }
+
+    public Ratings getAverageStarsByMovieId(String movieId) throws ExecutionException, InterruptedException {
+        Review[] reviews = getReviewByMovieId(movieId).toArray(new Review[0]);
+        int rating = 0;
+        for (Review review : reviews){
+            rating += review.getRating().getValue();
+        }
+        int reviewNum = reviews.length;
+        int avgRating = rating/reviewNum;
+
+        if(avgRating > 4.5){
+            return Ratings.FIVE;
+        } else if (avgRating < 4.5 && avgRating > 3.5) {
+            return Ratings.FOUR;
+        } else if (avgRating < 3.5 && avgRating > 2.5) {
+            return Ratings.THREE;
+        } else if (avgRating < 2.5 && avgRating > 1.5) {
+            return Ratings.TWO;
+        }else {
+            return Ratings.ONE;
+        }
     }
 
     public ArrayList<Review> getReviewByUserId(String userUID) throws ExecutionException, InterruptedException {
