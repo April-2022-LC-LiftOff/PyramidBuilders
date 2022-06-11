662f7656bc2d6bc6a3045c2e30ac641750ddae91
Bunny McIntyre <bunnymcintyre@Bunnys-MacBook-Pro.local>
2022-06-11 18:22:46 -0500

    added CSS styling to review. Need to get UserID to push with review; need to get user search for title to match with & save OMBD ID; need to get star rating to match with & save ENUM value

--- frontend/src/components/Reviews/CreateReview.js
+++ frontend/src/components/Reviews/CreateReview.js
@@ -113,8 +113,12 @@
 
 
 import React, { Component } from "react";
-import Form from "bootstrap";
-import { Link } from "react-router-dom";
+import StarRating from "./StarRating";
+import "./ReviewPageCSS.css";
+
+
+import { useNavigate } from 'react-router-dom';  
+import { Star } from "react-bootstrap-icons";
 
 export default class Review extends Component {
     handleSubmit = async (e) =>{
@@ -145,41 +149,31 @@ export default class Review extends Component {
 
             localStorage.setItem("token", token);  
 
-            window.location.replace('/profile')
+            // window.location.replace('/profile')
            
         } catch (err){
             console.log(err.message)
         }
+            
     }
         
     render() {
         return (  
-        <div >
-        <div >
-            <div className="HeaderContainer">
-            <div className="HeaderText">
-            Write a review! <br />
-                        </div>
-                        <div className="small">
-                            Fill in the form below
-                        </div>
-                    </div>    
-            <div >
-                <div >
-                </div>
-            </div>
-            <form className="Form FormContainer" onSubmit={this.handleSubmit}>
+        <div className="container">
+            <div className="header container"> Write a review </div>
+                       
+            <form className="FormContainer" >
                     <input type="text" name="movieId" onChange = {(e) => this.movieId = e.target.value}placeholder="Input movie ID" required/>
                     <input type="text" name="title" onChange = {(e) => this.title = e.target.value} placeholder="Give your review a title" required/>
 
-                    <input type="rating" name="rating" 
-                    onChange = {(e) => this.rating = e.target.value} className="Input username" placeholder="Rate the movie" />
+                    <div>Your rating: </div>
+                    <StarRating type="rating" name="rating" onChange = {(e) => this.rating = e.target.value}  />
+                    
                     <input type="text" name="reviewText" onChange = {(e) => this.reviewText = e.target.value}className="Input password" placeholder="Write your review here" required/>
                     
-                <button type="submit" name="btn" className="SubmitButton" onClick={this.handleSubmit}>Save Review</button>
+                <button type="submit" className="SubmitButton" onClick={this.handleSubmit}>Save Review</button>
                 </form>
         </div>
-    </div>
     )
 }
 }
