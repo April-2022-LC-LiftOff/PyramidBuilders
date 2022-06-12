116bdf0becd68fedbbf9dbb380b04058be9dc673
sjsbee <93555209+sjsbee@users.noreply.github.com>
2022-06-12 03:35:10 -0500

    Updated readme for GitHub Branches

---
+++ Screaming/reviewpage/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import ReviewPage from './components/ReviewPage';
+import ReviewSpots from './components/ReviewSpot';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ReviewPage/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
